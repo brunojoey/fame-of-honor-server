d79810246497a3b18c81efb21d40355069d2ba8f
Joey Bruno <brunojoey12@gmail.com>
2021-09-02 17:06:39 -0500

    Changed connection strings for ClearDB

--- database/connection.js
+++ database/connection.js
@@ -3,9 +3,9 @@ import mysql from "mysql";
 import util from "util"
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
+  host: "us-cdbr-east-04.cleardb.com",
+  user: "ba46ffa2322ebf",
+  password: "d9478934",
   database: "ringHonorDB"
 });
 
