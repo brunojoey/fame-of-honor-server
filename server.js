7f5b50bf1f1b02a6d6873c163f1d58bf5590de4a
Joey Bruno <brunojoey12@gmail.com>
2021-09-06 10:30:17 -0500

    Added index.html to sendFile function in server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use("/api/positions", positionRoutes);
 app.use(express.static(path.join(__dirname, "./dist")));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './dist', ))
+    res.sendFile(path.join(__dirname, './dist', "index.html"))
 });
 
 app.listen(PORT, function() {
