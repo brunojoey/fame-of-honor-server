cf3789f24f50972fe0c95df00e292e8092cab912
Joey Bruno <brunojoey12@gmail.com>
2021-09-22 10:39:47 -0500

    Took out the / our of the Origin header

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 connection; // Calls the Database
 
 app.use(function(req, res, next) {
-  res.header({"Access-Control-Allow-Origin": "http://www.thefameofhonors.com/"}); 
+  res.header({"Access-Control-Allow-Origin": "http://www.thefameofhonors.com"}); 
   res.header({"Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"});
   next();
 });
